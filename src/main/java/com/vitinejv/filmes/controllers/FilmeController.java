19e5994bb4f2e4ff4d6c3bd273a3cf8ed4615b01
Victor <victorgaspalmeida@hotmail.com>
2023-05-01 18:18:52 -0300

    put e regras de negocio

--- src/main/java/com/vitinejv/filmes/controllers/FilmeController.java
+++ src/main/java/com/vitinejv/filmes/controllers/FilmeController.java
@@ -3,8 +3,11 @@ package com.vitinejv.filmes.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,5 +34,15 @@ public class FilmeController {
 	@PostMapping
 	public void addFilmes(@RequestBody Filme f) {
 		services.addFilme(f);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public void deleteFilmes(@PathVariable Long id) {
+		services.deleteFilmes(id);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public void updateFilmes(@PathVariable Long id, @RequestBody Filme f) {
+		services.updateFilmes(id, f);
 	}
 }
--- src/main/java/com/vitinejv/filmes/services/FilmeServices.java
+++ src/main/java/com/vitinejv/filmes/services/FilmeServices.java
@@ -31,7 +31,51 @@ public class FilmeServices {
 		Optional<Filme> filmeOptional = repository.findByTitulo(f.getTitulo());
 		if(filmeOptional.isPresent()) {
 			throw new IllegalStateException("Já existe um filme com este nome");
+		} 
+		if (f.getTitulo() == null || !(f.getTitulo().length() > 0)) {
+			throw new IllegalStateException("Título inválido");
 		}
+		
+		if (!(f.getDuracao() > 0)) {
+			throw new IllegalStateException("Duração não pode ser igual ou menor que 0");
+		}
+
 		repository.save(f);
+	}
+
+	@Transactional
+	public void deleteFilmes(Long id) {
+		if(repository.existsById(id)) {
+			Filme f = repository.getReferenceById(id);
+			repository.delete(f);
+		}
+		else {
+			throw new IllegalStateException("filme com o id " + id + " não existe");
+		}
+	}
+
+	@Transactional
+	public void updateFilmes(Long id, Filme update) {
+		Filme f = repository.findById(id).orElseThrow(() -> 
+			new IllegalStateException("Filme com id " + id + " não existe"));
+		
+		if (update.getTitulo() != null && update.getTitulo().length() > 0
+				&& update.getTitulo() != f.getTitulo()) {
+			
+			Optional<Filme> optionalFilme = repository.findByTitulo(update.getTitulo());
+			if (optionalFilme.isPresent() && !update.getTitulo().equals(f.getTitulo())) {
+				throw new IllegalStateException("Já existe um filme com esse título");
+			}
+			f.setTitulo(update.getTitulo());
+		}
+		
+		if (update.getDuracao() > 0	&& update.getDuracao() != f.getDuracao()) {
+			f.setDuracao(update.getDuracao());
+		}
+		
+		if (update.getNota() != f.getNota()) {
+			f.setNota(update.getNota());
+		}
+		
 	}
 }
